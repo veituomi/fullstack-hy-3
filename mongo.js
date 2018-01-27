a9840e2b54954f3156601234121d9634715eb0df
Veini Tuominen <veini.tuominen@hotmail.com>
2018-01-27 16:22:57 +0200

    tehtävä 3.12

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const url = 'mongodb://fullstack:secret@ds217138.mlab.com:17138/fullstack-course';
+
+const params = process.argv.slice(2, process.argv.length);
+
+mongoose.connect(url);
+mongoose.Promise = global.Promise;
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema);
+
+let promise;
+
+if (params.length == 0) {
+  promise = Person
+    .find({})
+    .then(res => {
+      console.log('puhelinluettelo:')
+      res.forEach(person => {
+        console.log(person.name + ' ' + person.number);
+      });
+    });
+} else {
+  const [name, number] = params;
+
+  const person = new Person({
+    name,
+    number
+  });
+
+  promise = person
+    .save()
+    .then(response => {
+      console.log('lisätään henkilö ' + name + ' numero ' + number + ' luetteloon');
+    });
+}
+
+promise.then(() => {
+  mongoose.connection.close();
+});
